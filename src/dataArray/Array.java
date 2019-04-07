8f05074ff5239cc255f2d6c9b49a0a530e1f2bfb
Xiantaochen <1405818116@qq.com>
2019-04-08 00:10:09 +0800

    first commit

--- src/dataArray/Array.java
+++ src/dataArray/Array.java
@@ -1,12 +1,12 @@
 package dataArray;
 
-public class Array {
-    private int[] data;
+public class Array<E>{
+    private E[] data;
     private int size;
 
 
     public Array(int capacity){
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
         size = 0;
 
     }
@@ -30,7 +30,7 @@ public class Array {
         return size==0;
     }
 
-    public void addLast(int e){
+    public void addLast(E e){
         if(size == data.length) {
             throw new IllegalArgumentException("addlat failed , array is full");
         }
@@ -38,11 +38,11 @@ public class Array {
         size ++;
     }
 
-    public void addFirst(int e){
+    public void addFirst(E e){
         add(0,e);
     }
 
-    public void add(int index,int e){
+    public void add(int index,E e){
         if(size == data.length) {
             throw new IllegalArgumentException("addlat failed , array is full");
         }
@@ -56,32 +56,59 @@ public class Array {
         size ++;
     }
 
-    int get(int index){
-        if(inde < 0 || index >=size)
+    public E get(int index){
+        if(index < 0 || index >=size)
             throw new IllegalArgumentException("add failed , array is full");
         return data[index];
     }
 
-    void set(int index, int e){
-        if(inde < 0 || index >=size)
+    void set(int index, E e){
+        if(index < 0 || index >=size)
             throw new IllegalArgumentException("add failed , array is full");
         data[index] = e;
     }
 
-    public boolean contains(int e){
+    public boolean contains(E e){
         for(int i =0; i<size; i++){
-            if(data[i] == e)
+            if(data[i].equals(e))
                return true;
         }
         return false;
     }
 
-    public int find(int e){
+    public int find(E e){
         for(int i =0; i<size; i++){
-            if(data[i] == e)
+            if(data[i].equals(e))
                 return i;
         }
         return -1;
+    }
+
+    //删除
+
+    public E remove(int index){
+        if(index < 0 || index >=size)
+            throw new IllegalArgumentException("add failed , array is full");
+        E ret = data[index];
+        for (int i = index +1;i<size; i++)
+            data[i-1] = data[i];
+        size --;
+        data[size] = null;
+        return ret;
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+    public void removeElement(E e){
+        int index = find(e);
+        if(index !=-1)
+            remove(index);
     }
 
     @Override
